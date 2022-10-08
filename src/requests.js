4ec17f455ea28bb323acc0a1e7ede2d49fab89f9
Haseeb Ul Hassan <100109683+Haseeeb1@users.noreply.github.com>
2022-10-08 10:54:22 +0500

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "359e0dae159b75216dc4637baa1055ac";
+const API_KEY = "Your tmdb api key";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
